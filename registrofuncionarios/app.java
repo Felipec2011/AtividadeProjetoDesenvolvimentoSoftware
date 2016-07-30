fb6190cab2ced72c8465e19c644aee7f06a62b76
Felipec2011 <felipec2011@gmail.com>
2016-07-30 02:06:08 -0300

    Commit do trabalho

---
+++ registrofuncionarios/app.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package registrofuncionarios;
+
+/**
+ *
+ * @author Felipe Cavalcanti
+ */
+public class app {
+
+    /**
+     * Objetivo: Executar a classe.
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    } // Fim do main()
+    
+}
